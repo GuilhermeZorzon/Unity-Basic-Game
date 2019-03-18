1f45e6d6b92f78fa503cf31cad584c3a8aa7745d
GuilhermeZorzon <gui.zorzon@usp.br>
2019-03-18 15:36:55 -0300

    Added a scoring system based on the player position

---
+++ Basic_Game/Assets/Scenes/Scripts/Coin_Collector.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Coin_Collector : MonoBehaviour {
+	
+	public Transform player;
+	public Text score;
+
+	// Update is called once per frame
+	void Update () {
+		if(player.position.z >= 25.5)
+		score.text = (player.position.z - 25).ToString("0");
+	}
+}
