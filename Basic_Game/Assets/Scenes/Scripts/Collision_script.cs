43af57ab446a15b77342f493cf9d2bf8f342b951
GuilhermeZorzon <gui.zorzon@usp.br>
2019-03-18 15:05:31 -0300

    commiting the game in it's initial state

---
+++ Basic_Game/Assets/Scenes/Scripts/Collision_script.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision_script : MonoBehaviour {
+
+	public PlayerMovement movement;
+
+	// Used when collision occurs
+
+	void OnCollisionEnter (Collision collisioninfo) {
+
+		if (collisioninfo.gameObject.CompareTag("Obstacle")){
+			movement.enabled = false;
+		}
+
+	}
+}
---
+++ Basic_Game/Assets/Scenes/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public Rigidbody rb;
+
+	public float forwardForce = 2000f;
+
+	public float sidewayForce = 500f;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame. FixedUpdate is because of Physics
+	void FixedUpdate () {
+		
+		rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+		if( Input.GetKey("d") ){
+
+			rb.AddForce(sidewayForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+		}
+
+		else if ( Input.GetKey("a") ){
+			
+			rb.AddForce(-sidewayForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+		}
+	}
+}
