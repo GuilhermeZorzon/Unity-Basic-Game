f23d837ef34bc8dfb3f66d0d24e3d5789151e5c4
GuilhermeZorzon <gui.zorzon@usp.br>
2019-03-19 13:23:55 -0300

    Made one more phase and changed the other 2. Added a Welcome and a Credits menu with it's buttons

---
+++ Basic_Game/Assets/Scenes/Scripts/Credits_script.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Credits_script : MonoBehaviour {
+
+	public void QuitGame () {
+
+		Debug.Log("Quit!");
+		Application.Quit();
+	}
+
+	public void StartGame () {
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+
+	public void RestartGame () {
+		SceneManager.LoadScene(0);
+	}
+
+}
--- Basic_Game/Assets/Scenes/Scripts/LevelLoad.cs
+++ Basic_Game/Assets/Scenes/Scripts/LevelLoad.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class LevelLoad : MonoBehaviour {
 
 	public void LoadNextLevel () {
-		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);;
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 	}
 
 }
